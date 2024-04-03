37a5a1c84b7bdc58ab0f6c9516fb5ee7a2230bd1
GinusHR <1074730@hr.nl>
2024-04-03 12:38:19 +0200

    begonnen met webservice

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -1,12 +1,25 @@
 <?php
 
-function getMuseum (){
+function getMuseum()
+{
     return [
         [
-        "id" => 1,
-        "name" => 'Het Rijksmuseum',
-        "description" => 'Big museum',
-        "img" => 'images/Rijksmuseum.jpg'
+            "id" => 1,
+            "name" => 'Het Rijksmuseum',
+            "description" => 'Big museum',
+            "img" => 'images/Rijksmuseum.jpg'
+        ],
+        [
+            "id" => 2,
+            "name" => 'Kunsthal',
+            "description" => 'Kunst museum',
+            "img" => 'images/kunsthal.jpeg'
+        ],
+        [
+            "id" => 3,
+            "name" => 'Archeon',
+            "description" => 'Open lucht museum',
+            "img" => 'images/Archeon.JPG'
         ]
     ];
 }
