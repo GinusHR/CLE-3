a70634889fdda978375b1f227d5dff6f31abc9a6
GinusHR <1074730@hr.nl>
2024-04-04 13:10:07 +0200

    Buttons banger

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -8,6 +8,7 @@ function getMuseum()
             "name" => 'Het Rijksmuseum',
             "tag" => 'Big museum',
             "img" => 'images/Rijksmuseum.jpg',
+            "alt" => 'Groot, mooi en oud gebouw in Amsterdam',
             "description" => 'Het Rijksmuseum in Amsterdam is een van de zestien rijksmusea in Nederland. De collectie biedt een overzicht van de Nederlandse kunst en geschiedenis met onder andere werken van 17e-eeuwse Nederlandse meesters als Rembrandt, Vermeer en Hals. Het museum is sinds 1885 gevestigd in het Rijksmuseumgebouw dat ontworpen werd door de Nederlandse architect Pierre Cuypers'
         ],
         [
@@ -15,6 +16,7 @@ function getMuseum()
             "name" => 'Kunsthal',
             "tag" => 'Kunst museum',
             "img" => 'images/kunsthal.jpeg',
+            "alt" => '',
             "description" => 'De Kunsthal Rotterdam, geopend in 1992, is een expositieruimte in Rotterdam. Het is de eerste Kunsthal van Nederland. Het gebouw is gelegen in het Museumpark en heeft een ingang aan de Westzeedijk tegenover het Koningin Emmaplein. Naast de Kunsthal bevindt zich het Natuurhistorisch Museum Rotterdam.'
         ],
         [
