b4c4547cbdfcccee7cf9c8dc4d1178e4be13e5f6
GinusHR <1074730@hr.nl>
2024-04-08 13:24:19 +0200

    doomguy

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -6,7 +6,7 @@ function getMuseum()
         [
             "id" => 1,
             "name" => 'Het Rijksmuseum',
-            "tag" => 'Big museum',
+            "tag" => 'Iconisch museum van Asterdam',
             "img" => 'images/Rijksmuseum.jpg',
             "alt" => 'Groot, mooi en oud gebouw in Amsterdam',
             "description" => 'Het Rijksmuseum in Amsterdam is een van de zestien rijksmusea in Nederland. De collectie biedt een overzicht van de Nederlandse kunst en geschiedenis met onder andere werken van 17e-eeuwse Nederlandse meesters als Rembrandt, Vermeer en Hals. Het museum is sinds 1885 gevestigd in het Rijksmuseumgebouw dat ontworpen werd door de Nederlandse architect Pierre Cuypers'
@@ -14,7 +14,7 @@ function getMuseum()
         [
             "id" => 2,
             "name" => 'Kunsthal',
-            "tag" => 'Kunst museum',
+            "tag" => 'Museum met vele soorten kunst',
             "img" => 'images/kunsthal.jpeg',
             "alt" => 'Modern gebouw van wit steen en glas. Vierkant.',
             "description" => 'De Kunsthal Rotterdam, geopend in 1992, is een expositieruimte in Rotterdam. Het is de eerste Kunsthal van Nederland. Het gebouw is gelegen in het Museumpark en heeft een ingang aan de Westzeedijk tegenover het Koningin Emmaplein. Naast de Kunsthal bevindt zich het Natuurhistorisch Museum Rotterdam.'
@@ -22,7 +22,7 @@ function getMuseum()
         [
             "id" => 3,
             "name" => 'Archeon',
-            "tag" => 'Open lucht museum',
+            "tag" => 'Museum over de geschiedenis',
             "img" => 'images/Archeon.JPG',
             "alt" => 'Ouderwets Romeins geïnspireerd gebouw.',
             "description" => 'Museumpark Archeon is een openluchtmuseum en educatief belevingspark in het Zuid-Hollandse Alphen aan den Rijn dat het leven in Nederland tijdens de prehistorie, Romeinse tijd en middeleeuwen laat zien. Het maakt daarbij gebruik van archeologische reconstructies, het naspelen van dagelijkse bezigheden waar bezoekers aan kunnen deelnemen (levende geschiedenis) en het tentoonstellen van originele historische voorwerpen. Het park kreeg in 2015 de museumstatus.'
